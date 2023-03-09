d1b0db284ff42a186d964cb73af73c1b36bffc06
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-03-09 18:24:16 -0300

    Aulas 1, 2 ,3

---
+++ aula3.py
@@ -0,0 +1,29 @@
+'''
+    Python = Linguagem de programação
+    Tipo de tipagem = Dinâmica / Forte
+    str -> string -> texto
+    String são textos que estão dentro de aspas
+'''
+
+
+print(1234)
+print(1234)
+print(1234)
+
+#Aspas simples
+
+print('1234')
+print(1, "1234 '567'")
+
+#Aspas duplas
+
+print("1234")
+print(2, '1234 "567"')
+
+#Escape
+
+print("1234 \"567\"")
+
+#r
+
+print(r"1234 \"567\"")
