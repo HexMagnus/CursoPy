175f180b7d55077555782c43f54db77454d7e1cf
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-03-16 19:35:44 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,11 @@
+#Tipos int e float
+# int -> Número inteiro
+# O tipo int  representa qualquer nunumero
+# positivo ou negativo. int sem sinal é considerado positivo
+
+print(11) # int
+print(-11) # int
+print(0) # int
+
+
+
