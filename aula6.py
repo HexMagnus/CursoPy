4b565614448a2a84e3e332dde6cd28fae75f192c
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2023-03-16 20:11:55 -0300

    Create aula6.py

---
+++ aula6.py
@@ -0,0 +1,10 @@
+# coversão de tipos , coerrsão
+# type convertionn, typeccasting, coercion
+# é o ato de conveerter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+print(1 + 1)
+print("a" + "b")
+print(int("1"), type(int('1')))
+print(float('1') + 1) 
+print(bool(''))
